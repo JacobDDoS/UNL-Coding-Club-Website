93d75250c55d4794f5f7c90923b32d4e7e335036
toschaef <notthomasschaefer@gmail.com>
2025-08-18 18:14:35 -0500

    tailwind theme
    - added color scheme to globals.css
    - fixed redirects in components/header/NavPill.tsx
    - changed text color in components/header/LoginButton.tsx
    - updated images in /public

--- uccw/src/components/header/NavPill.tsx
+++ uccw/src/components/header/NavPill.tsx
@@ -1,24 +1,30 @@
 'use client'
 import Image from "next/image"
+import { redirect } from "next/navigation";
 
 export default function NavPill() {
+  const direct = (link: string): void => {
+    window.open(link, '_blank');
+  }
+
   return (
     <div className='flex items-center gap-x-8 justify-center px-6 py-3 bg-[#353535] rounded-xl'>
       {/* left section: home - other thing */}
       <div className='relative w-5 h-5'>
         <Image
-          src='/home.png'
+          src='/house.svg'
           alt='Home'
           fill
+          onClick={() => redirect('/')}
           className='hover:brightness-75'
         />
       </div>
-      <div className='relative w-6 h-6'>
+      <div className='relative w-5 h-5'>
         <Image
-          src='/community.png'
+          src='/users.svg'
           alt='Subclubs'
           fill
-          onClick={() => window.open('https://gmail.com', '_blank')}
+          onClick={() => redirect('/about')}
           className='hover:brightness-75'
         />
       </div>
@@ -32,25 +38,25 @@ export default function NavPill() {
           src='/github.webp'
           alt='GitHub'
           fill
-          onClick={() => window.open('https://github.com/JacobDDoS/UNL-Coding-Club-Website', '_blank')}
-          className='hover:brightness-75'
-        />
-      </div>
-      <div className='relative w-5 h-5'>
-        <Image
-          src='/ig.webp'
-          alt='Instagram'
-          fill
-          onClick={() => window.open('https://www.instagram.com/', '_blank')}
+          onClick={() => direct('https://github.com/JacobDDoS/UNL-Coding-Club-Website')}
           className='hover:brightness-75'
         />
       </div>
       <div className='relative w-6 h-6'>
         <Image
-          src='/discord.webp'
+          src='/instagram.svg'
+          alt='Instagram'
+          fill
+          onClick={() => direct('https://www.instagram.com/unlcodingclub')}
+          className='hover:brightness-75'
+        />
+      </div>
+      <div className='relative w-6 h-6'>
+        <Image
+          src='/discord.svg'
           alt='Discord'
           fill
-          onClick={() => window.open('https://discord.gg/BpREX2D5Eg', '_blank')}
+          onClick={() => direct('https://discord.gg/BpREX2D5Eg')}
           className='hover:brightness-75'
         />
       </div>

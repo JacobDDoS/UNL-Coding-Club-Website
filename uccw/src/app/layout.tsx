25fd5c3e5dbd9909c866e4f2a471269edf8b75c2
JacobDDoS <jacdimmitt@gmail.com>
2025-09-08 13:43:17 -0500

    fix(layout): remove uncessary variables from SSR

--- uccw/src/app/layout.tsx
+++ uccw/src/app/layout.tsx
@@ -3,16 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "@/src/app/globals.css";
 import Header from "@/src/components/header/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "UNL Coding Club",
   description: "Official UNL Coding Club",
@@ -26,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-bg-primary`}
+        className={`--font-geist-sans --font-geist-mono antialiased bg-bg-primary`}
       >
         <Header />
         {children}

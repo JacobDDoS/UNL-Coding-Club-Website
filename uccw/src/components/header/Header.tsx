74fdf9f3bd6be3952937ea90d7326f6f7e6d77d5
JacobDDoS <jacdimmitt@gmail.com>
2025-09-10 13:53:59 -0500

    refac(header): update CSS interactivity with buttons

--- uccw/src/components/header/Header.tsx
+++ uccw/src/components/header/Header.tsx
@@ -7,8 +7,7 @@ export default function Header() {
   return (
     <div>
       <div className='flex flex-row justify-center'>
-        <div className='flex flex-row items-center justify-evenly px-10 my-2 w-full max-w-[1100px] h-1/5 min-h-[65px] bg-bg-primary'>
-        
+        <div className='flex flex-row items-center justify-evenly px-10 my-2 w-full gap-40 max-w-[1400px] h-1/5 min-h-[65px] bg-bg-primary'>
           <LeftLinks />
           <Logo />
           <RightLinks />
--- uccw/src/components/header/LeftLinks.tsx
+++ uccw/src/components/header/LeftLinks.tsx
@@ -1,24 +1,21 @@
-'use client'
-import { redirect } from "next/navigation";
-
 export default function LeftLinks() {
   return (
-    <div className='flex flex-row w-1/3 justify-between items-center font-bold text-l text-white'>
-      <button
-        onClick={() => redirect('/about')}
-      >
-        About
-      </button>
-      <button
-        onClick={() => redirect('/resources')}
-      >
-        Resources
-      </button>
-      <button
-        onClick={() => redirect('/subclubs')}
-      >
-        Subclubs
-      </button>
+    <div className='flex flex-row w-1/3 justify-between items-center font-bold text-l text-xl text-white'>
+      <a href="/about">
+        <button className="cursor-pointer transition ease-in duration-300 hover:underline decoration-[var(--color-secondary)] hover:scale-105">
+          About
+        </button>
+      </a>
+      <a href="/resources">
+        <button className="cursor-pointer transition ease-in duration-300 hover:underline decoration-[var(--color-secondary)] hover:scale-105">
+          Resources
+        </button>
+      </a>
+      <a href="/subclubs">
+        <button className="cursor-pointer transition ease-in duration-300 hover:underline decoration-[var(--color-secondary)] hover:scale-105">
+          Subclubs
+        </button>
+      </a>
     </div>
   );
 }

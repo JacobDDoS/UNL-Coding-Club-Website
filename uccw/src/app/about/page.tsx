f826a771ec6c83d14885fac7e92c6fd6f36af531
ethanol25 <ethandye04@gmail.com>
2025-09-19 14:31:02 -0500

    about page details

--- uccw/src/app/about/page.tsx
+++ uccw/src/app/about/page.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image';
 import Blocks from "@/src/components/page/Blocks";
 
-//<div className="absolute -inset-4 bg-gradient-to-r from-red-600 to-bg-primary rounded-2xl blur-sm opacity-70 -z-10"></div>
+//                  <div className=" bg-gradient-to-r from-secondary backdrop-opacity-70 -z-10 -translate-x-6" style={{clipPath: "polygon(0% 0%, 2% 25%, 3% 50%, 2% 75%, 0% 100%, 100% 100%, 100% 0%)"}}>
+//                   <svg className="absolute" width="740" height="152" viewBox="0 0 740 152" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17.1614 10.9846C15.4494 4.48566 9.48217 0.0103874 2.76164 0.0463104C-13.5741 0.133629 45.1829 0.0604272 109.92 0H627.082C682.491 4.37443 735.462 61.564 739.724 137.383C740.18 145.498 733.887 151.987 725.759 151.987C623.204 151.991 -0.641047 152.014 14.1486 151.987C29.6448 151.958 40.9883 101.434 17.1614 10.9846Z" fill="url(#paint0_linear_559_3)"/><defs><linearGradient id="paint0_linear_559_3" x1="190.348" y1="409.451" x2="602.94" y2="-232.3" gradientUnits="userSpaceOnUse"><stop offset="0.110577" stop-color="#ED1C24"/><stop offset="0.831731" stop-color="#262626" stop-opacity="0"/></linearGradient></defs></svg>
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
                     <h1 className='text-5xl text-balance text-white font-extrabold drop-shadow-lg/50 leading-snug translate-x-6'>
                       Learning by Doing, One Project at a Time.
                     </h1>
-                  </div>
+                  </div>  
                   <p className="text-white text-2xl font-semibold max-w-xl pt-4 drop-shadow-lg/50">
                     Made in the Fall of 2024, Coding Club aims to create a collaborative environment where students can learn, build, and explore in the field of Computer Science.
                   </p>

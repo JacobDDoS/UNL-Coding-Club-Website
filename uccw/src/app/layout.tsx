c91f0b8171c1be41d05dca2bb4298ec867aa1bcf
ethanol25 <ethandye04@gmail.com>
2025-09-20 17:55:09 -0500

    Changed font to Inter

--- uccw/src/app/layout.tsx
+++ uccw/src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "@/src/app/globals.css";
 import Header from "@/src/components/header/Header";
 import Blocks from "@/src/components/page/Blocks";
 
+const inter = Inter({subsets:['latin']});
 
 export const metadata: Metadata = {
   title: "UNL Coding Club",
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="min-h-screen">
       <body
-        className="--font-geist-sans --font-geist-mono antialiased bg-bg-primary min-h-screen bg-linear-to-b from-[#1E1E1E]/[.1] to-[#2E2E2E]"
+        className={`${inter.className} antialiased bg-bg-primary min-h-screen bg-linear-to-b from-[#1E1E1E]/[.1] to-[#2E2E2E]`}
       >
         <Header />
         <div className="select-none min-h-[calc(90vh-50px)] w-[calc(100%-50px)] flex justify-center rounded-2xl m-auto relative shadow-[inset_0_0_15px_rgba(0,0,0,0.5)] shadow-white">
